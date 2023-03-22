4c38dc5e20335d53f2443bb201b1ba8cf6a1c5bd
zeeshaantandel <zeeshaan143tandel@gmail.com>
2023-03-23 02:37:20 +0530

    Initial commit (2)

---
+++ app/src/main/java/com/example/lgbtqdatingapp/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lgbtqdatingapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lgbtqdatingapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
